9a150105a90e4b9f98e389596252f6c09ee98a0d
Chengxi Ye <cxy@umiacs.umd.edu>
2016-05-17 09:45:48 -0400

    20160517

--- CoreModules/tanh_ln.m
+++ CoreModules/tanh_ln.m
@@ -1,7 +1,7 @@
 function y = tanh_ln(x,dzdy)
 
     if nargin <= 1 || isempty(dzdy)
-        y = (exp(x)-exp(-x)) ./ (exp(x)+exp(-x));
+        y = tanh(x);
     else
         y = 4./(exp(x)+exp(-x)).^2;
     end
