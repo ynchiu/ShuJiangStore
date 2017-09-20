7712683e8a2ee1a63278296cd3f90e1aff37b69d
yuning.qiu <ynchiu@126.com>
2017-09-21 00:15:27 +0800

    changes
    delete some unusable.

--- CoreModules/relu.m
+++
@@ -1,9 +0,0 @@
-function y = relu(x,dzdy)
-
-  if nargin <= 1 || isempty(dzdy)
-    y = max(x, single(0)) ;
-  else
-    y = dzdy .* (x > single(0)) ;
-  end
-  
-end
--- CoreModules/tanh_ln.m
+++
@@ -1,9 +0,0 @@
-function y = tanh_ln(x,dzdy)
-
-    if nargin <= 1 || isempty(dzdy)
-        y = tanh(x);
-    else
-        y = dzdy.*(4./(exp(x)+exp(-x)).^2);
-    end
-
-end
